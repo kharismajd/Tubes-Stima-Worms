a8bce7a5e4a03df35a4ec5de4b516ede90abb121
Allief Nuriman <57051116+allief876@users.noreply.github.com>
2021-02-12 08:22:02 +0700

    Nambahin BananaCommand.java & SnowballCommand.java

---
+++ src/main/java/za/co/entelect/challenge/command/BananaCommand.java
@@ -0,0 +1,19 @@
+package za.co.entelect.challenge.command;
+
+import za.co.entelect.challenge.enums.Direction;
+
+public class BananaCommand implements Command {
+
+    private int x;
+    private int y;
+
+    public BananaCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String render() {
+        return String.format("banana %d %d", this.x, this,y);
+    }
+}
